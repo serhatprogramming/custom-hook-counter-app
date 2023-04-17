e42ada3352adfbe216526c8b5b395285b8c1b2bb
serhatprogramming <serhatprogramming@gmail.com>
2023-04-17 17:09:05 -0400

    custom hook added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { useState } from "react";
 
+const useCounter = () => {
+  const [value, setValue] = useState(0);
+
+  const increase = () => {
+    setValue(value + 1);
+  };
+  const decrease = () => {
+    setValue(value - 1);
+  };
+  const reset = () => {
+    setValue(0);
+  };
+
+  return { value, increase, decrease, reset };
+};
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const counter = useCounter();
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter - 1)}>-</button>
-      <button onClick={() => setCounter(0)}>reset</button>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <div>{counter.value}</div>
+      <button onClick={counter.decrease}>-</button>
+      <button onClick={counter.reset}>reset</button>
+      <button onClick={counter.increase}>+</button>
     </div>
   );
 };
